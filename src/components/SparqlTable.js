5ac8be1fb1b80d9f1d6766854ad69ec9dca22c0d
franekmagiera <framagie@gmail.com>
2020-12-22 16:09:55 +0100

    Present data from SPARQL queries in a table

--- src/components/SparqlTable.js
+++ src/components/SparqlTable.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import { Table } from 'reactstrap';
 
 const SparqlTable = (props) => {
-    return (
-        <div>
-            <p>{JSON.stringify(props)}</p>
-        </div>
-    );
+
+    if (props.data) {
+        const columnNames = props.data.head.vars;
+        const data = props.data.results.bindings;
+
+        const createTableData = (value) => {
+            const tableData = (value.type === 'uri') ? <a href={value.value} target='_blank' rel='noopener noreferrer'>{value.value}</a> : value.value;
+            return <td>{tableData}</td>;
+        }
+        
+        return (
+            <div>
+                <Table borderless responsive size='sm'>
+                    <thead>
+                        <tr>
+                            {columnNames.map(columnName => <th>{columnName}</th>)}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data.map(obj => <tr>{Object.values(obj).map(createTableData)}</tr>)}
+                    </tbody>
+                </Table>
+            </div>
+        );
+    } else {
+        return <div></div>;
+    }
 };
 
 export default SparqlTable;
