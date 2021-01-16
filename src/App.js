737101cfafd6526016100d52a276f03d50d36103
franekmagiera <framagie@gmail.com>
2021-01-16 16:35:19 +0100

    Resize SPARQL query form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Container, Row, Col, Button } from 'reactstrap';
 import Header from './components/Header.js';
 import LeftPane from './components/LeftPane.js';
 import RightPane from './components/RightPane.js';
+import DataSourceEnum from './components/DataSourceEnum.js';
 
 function App() {
   const [data, setData] = useState(null);
@@ -19,18 +20,29 @@ function App() {
     link.click();
   };
 
+  const sizeMapping = Object.freeze({
+    [DataSourceEnum.SEARCH_ENTITIES]: {
+      first: '3',
+      second: '9'
+    },
+    [DataSourceEnum.SPARQL_QUERY]: {
+      first: '6',
+      second: '6'
+    }
+  });
+
   return (
     <div>
       <Header />
       <Container fluid>
         <Row>
-          <Col sm='3' className='leftPaneCol'>
+          <Col sm={dataSourceType ? sizeMapping[dataSourceType].first : '3'} className='leftPaneCol'>
               <LeftPane setData={setData} onDataSourceChange={setDataSourceType} />
               <div className='exportButton'>
                 <Button color='secondary' onClick={onExportClick} disabled={!data} block>Export</Button>
               </div>
           </Col>
-          <Col sm='9' className='rightPanelCol'>
+          <Col sm={dataSourceType ? sizeMapping[dataSourceType].second : '9'} className='rightPanelCol'>
               <RightPane data={data} dataSourceType={dataSourceType} />
           </Col>
         </Row>
